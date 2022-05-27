d72c3e815f25e47acdf6eb5a6d19b8301697a4be
zhaowen <dyzxhduzw@163.com>
2022-05-27 22:03:04 +0800

    最长有效括号

---
+++ Summarize/栈与队列/括号问题/02有效的括号字符串.go
@@ -0,0 +1,64 @@
+package 括号问题
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+/**
+ * @Description
+给定一个只包含三种字符的字符串：（，）和 *，写一个函数来检验这个字符串是否为有效字符串。有效字符串具有如下规则：
+
+1.任何左括号 (必须有相应的右括号 )
+2.任何右括号 )必须有相应的左括号 (
+3.左括号 ( 必须在对应的右括号之前 )
+4.*可以被视为单个右括号 )，或单个左括号 (，或一个空字符串
+5.一个空字符串也被视为有效字符串
+
+输入: "(*)"
+输出: True
+示例 3:
+
+输入: "(*))"
+输出: True
+ * @Author 赵稳
+ * @Date 2022-05-27 15:56
+ **/
+
+/*
+两个栈解决
+*/
+func checkValidString(s string) bool{
+       var leftStk []int     //存放左括号 下标 的栈
+	   var asteriskStk []int //存放星号 下标 的栈
+	   for i,ch:=range s{
+		   if ch=='('{
+			   leftStk=append(leftStk,i)
+		   }else if ch=='*'{
+			   asteriskStk =append(asteriskStk,i)
+		   }else if len(leftStk)>0{ //遇到右括号，则保持左括号下标的栈 出栈
+			   leftStk=leftStk[:len(leftStk)-1]
+		   }else if len(asteriskStk)>0{ //如果没有左括号了，则到保持星号下标的栈中寻找
+			   asteriskStk = asteriskStk[:len(asteriskStk)-1]
+		   }else{
+			   return false
+		   }
+	   }
+	   //遍历结束之后，左括号栈和星号栈可能还有元素:要满足下列两个规则：
+	   //为了将每个左括号匹配，需要将星号看成右括号
+	   //且每个左括号必须出现在其匹配的星号之前
+	   i:=len(leftStk)-1
+	   for j:=len(asteriskStk)-1;i>=0&&j>=0;i,j=i-1,j-1{
+		   if leftStk[i]>asteriskStk[j]{ //如果左括号出现在星号之后，则不能匹配 *（=）（
+			   return false
+		   }
+	   }
+        return i<0 //最后应该栈为空
+}
+func  checkValidStringTest(){
+	input:=bufio.NewScanner(os.Stdin)
+	input.Scan()
+	str:=input.Text()
+	fmt.Println(checkValidString(str))
+}
---
+++ Summarize/栈与队列/括号问题/03最长有效括号.go
@@ -0,0 +1,68 @@
+package 括号问题
+
+/**
+ * @Description  https://github.com/dyzxzw/LeetCodeSummarize.git
+给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+
+输入：s = "(()"
+输出：2
+解释：最长有效括号子串是 "()"
+示例 2：
+
+输入：s = ")()())"
+输出：4
+解释：最长有效括号子串是 "()()"
+
+ * @Author 赵稳
+ * @Date 2022-05-27 21:38
+ **/
+
+/*
+利用标记数组mark 记录连续0的个数，这个数量就是最长有效括号的长度
+
+s = ")()())"
+
+mark 100001
+
+
+*/
+func longestValidParentheses(s string) int {
+         st:=make([]int,0)  //记录每个括号的下标栈，如果是左括号则入栈，右括号则出栈
+		 mark:=make([]int,len(s)) //标记数组，初始全为0
+		 for i:=0;i<len(s);i++{
+			 if s[i]=='('{
+				 st=append(st,i)
+			 }else{
+				 if len(st)==0{
+					 mark[i]=1 //如果栈为空，且为右括号，则直接在mark数组对应位置标记为1
+				 }else{
+					 st=st[:len(st)-1] //左右括号匹配上了，出栈即可
+				 }
+			 }
+		 }
+	//如果栈中还存有没有匹配完的，说明剩下的括号都是无法匹配的
+	//直接在对应mark的位置标记为1
+	for len(st)>0{
+		mark[st[len(st)-1]]=1
+		st=st[:len(st)-1] //出栈
+	}
+	//统计连续0的个数
+	cnt,maxCnt:=0,0
+	for _,v:=range mark{
+		if v==1{
+			cnt=0
+			continue
+		}
+		cnt++
+		maxCnt=max(maxCnt,cnt)
+	}
+	return maxCnt
+}
+
+func max(a,b int)int{
+	if a>b{
+		return a
+	}else{
+		return b
+	}
+}
